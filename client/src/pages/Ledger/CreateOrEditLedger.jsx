f9f90ba8cc8c3bde9cded7bfdc767f3be25fa456
suraj-kmr <srjkmr431@gmail.com>
2025-07-13 13:50:14 +0530

    Updates css

--- client/src/pages/Ledger/CreateOrEditLedger.jsx
+++ client/src/pages/Ledger/CreateOrEditLedger.jsx
@@ -4,7 +4,7 @@ import { useNavigate, useLocation } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { adminRoute } from '../../utils/router';
 
-const transactionTypes = ['debit', 'credit'];
+const transactionTypes = ['deposit', 'Withdrawn', 'Interest'];
 
 const initialParticulars = [
     'Office Equipment',
